8ad3de4462cb6b3e239e00b0728fd69e47af67df
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-06 07:35:16 +0000

    增加连接错误的提示 修改DEBUG，错误弹出框

--- trunk/TankClient/src/lc/client/util/Debug.java
+++ trunk/TankClient/src/lc/client/util/Debug.java
@@ -1,11 +1,19 @@
 package lc.client.util;
 
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import lc.client.ui.frame.MainFrame;
+
 /**
  * 调试类 用于打印服务端信息
  * @author LUCKY
  *
  */
 public class Debug {
+	public static final ImageIcon errorIcon=new ImageIcon(Toolkit.getDefaultToolkit().createImage(Class.class.getResource("/images/error_dialog_icon.png")).getScaledInstance(73, 82, 1));
 	
 	/**
 	 * 输出服务端信息
@@ -21,7 +29,9 @@ public class Debug {
 	 * @param e
 	 */
 	public static void error(Object obj,Exception e){
+		JOptionPane.showInternalMessageDialog(MainFrame.getInstance().getContentPane(), obj+"\n"+e.getMessage(),"提示",JOptionPane.ERROR_MESSAGE,errorIcon);
 		System.out.println(obj);
+		e.printStackTrace();
 	}
 	
 }
