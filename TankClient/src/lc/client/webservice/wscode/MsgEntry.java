d10b96f3debb64de01d595c24bb66a6377a8c223
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-07 12:29:07 +0000

    wsimport生成的客户端代码

---
+++ TankClient/src/lc/client/webservice/wscode/MsgEntry.java
@@ -0,0 +1,79 @@
+
+package lc.client.webservice.wscode;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for msgEntry complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="msgEntry">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="result" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
+ *         &lt;element name="resultMessage" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "msgEntry", propOrder = {
+    "result",
+    "resultMessage"
+})
+public class MsgEntry {
+
+    protected boolean result;
+    protected String resultMessage;
+
+    /**
+     * Gets the value of the result property.
+     * 
+     */
+    public boolean isResult() {
+        return result;
+    }
+
+    /**
+     * Sets the value of the result property.
+     * 
+     */
+    public void setResult(boolean value) {
+        this.result = value;
+    }
+
+    /**
+     * Gets the value of the resultMessage property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getResultMessage() {
+        return resultMessage;
+    }
+
+    /**
+     * Sets the value of the resultMessage property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setResultMessage(String value) {
+        this.resultMessage = value;
+    }
+
+}
