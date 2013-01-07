63301d8a513f494f1c50edf01236a6e38dec05dc
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-07 01:32:45 +0000

    在关闭窗口时增加逻辑， 一直监测到主线程开始运行了才进行关闭

--- trunk/TankClient/src/lc/client/ui/components/LDialog.java
+++ trunk/TankClient/src/lc/client/ui/components/LDialog.java
@@ -14,6 +14,7 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLayeredPane;
 
 import lc.client.ui.frame.MainFrame;
+import lc.client.util.Debug;
 
 /**
  * 私有Dialog
@@ -25,6 +26,8 @@ public abstract class LDialog extends JInternalFrame implements ILWindow{
 	private static final long serialVersionUID = 1L;
 	private static final String START_LWMODAL="startLWModal";
 	private static final String STOP_LWMODAL="stopLWModal";
+	
+	private Thread popUpThread;
 
 	/**
 	 * 私有DIALOG
@@ -73,6 +76,7 @@ public abstract class LDialog extends JInternalFrame implements ILWindow{
 	 * 打开窗口
 	 */
 	public void popUp() {
+		popUpThread=Thread.currentThread();//上次打开它的线程（用来检测模态是否结束）
 		onPopUp();
 		this.setVisible(true);
 		setLWModal(true);//启动模态
@@ -82,11 +86,41 @@ public abstract class LDialog extends JInternalFrame implements ILWindow{
 	 * 关闭窗口
 	 */
 	public void closeDialog(){
+		//某些情况下可能会出现唤醒失败的问题！
 		setLWModal(false);//关闭模态
+		/**
+		 * 由于关闭模态的时候进行的是线程相关的操作
+		 * 所以有可能出现阻塞，死锁等状况导致模态在很长时间里无法结束，造成主线程无限WAITING，主界面无法操作。
+		 * 所以在这里增加逻辑，每0.1秒监测一下主线程是不是在运行态，如果没有则继续监测
+		 * 如果监测了30次（3秒钟）主线程还没有重新启动运行，则认为模态关闭失败。
+		 * 此时就手动唤醒主线程，然后再次调用关闭窗口方法来关闭LOADING的模态，防止主线程无限WAITING。
+		 */
+		int count=0;
+		while(!Thread.State.RUNNABLE.equals(popUpThread.getState())){
+			try {
+				Thread.sleep(100);
+				if(count++==10){//如果三秒钟都没有结束模态，则重新调用 模态停止方法
+					synchronized(popUpThread){
+						popUpThread.notify();
+					}
+					closeDialog();
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		this.setVisible(false);
 	}
 	
-
+	/**
+	 * 获得最近一次弹出该模态窗口的线程
+	 * @return
+	 */
+	protected Thread getLastPopUpThread(){
+		return popUpThread;
+	}
+	
 	/**
 	 * 按钮动作监听
 	 */
@@ -113,11 +147,14 @@ public abstract class LDialog extends JInternalFrame implements ILWindow{
 		String methodName;
 		if(isModal){
 			methodName=START_LWMODAL;
+			this.setDialogSelected(true);
 		}else{
 			methodName=STOP_LWMODAL;
+			this.setDialogSelected(false);
 		}
 		try {
             Object obj;
+            Debug.debug(methodName);
 			obj = AccessController.doPrivileged(new ModalPrivilegedAction(Container.class, methodName));//启用特权
             if (obj != null) {
             	((Method)obj).invoke(this, (Object[])null);
@@ -125,11 +162,6 @@ public abstract class LDialog extends JInternalFrame implements ILWindow{
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-		if(isModal){//选中
-			this.setDialogSelected(true);
-		}else{
-			this.setDialogSelected(false);
-		}
 	}
 	
 	/**
