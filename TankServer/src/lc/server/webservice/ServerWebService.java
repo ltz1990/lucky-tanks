ae8e88cd7a3b7e7da77db025dcfee0c591fca8ff
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-07 08:15:57 +0000

    增加注释

--- TankServer/src/lc/server/webservice/ServerWebService.java
+++ TankServer/src/lc/server/webservice/ServerWebService.java
@@ -2,10 +2,28 @@ package lc.server.webservice;
 
 import javax.jws.WebService;
 
+/**
+ * webservice½Ó¿Ú
+ * @author LUCKY
+ *
+ */
 @WebService
 public interface ServerWebService {
+	
+	/**
+	 * µÇÂ½
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	public MsgEntry login(String username,String password);
 	
+	/**
+	 * ×¢²á
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	public MsgEntry register(String username,String password);
 
 }
