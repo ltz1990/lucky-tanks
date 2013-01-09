95fd0056149197ba6d870987838754de7b0c0f7d
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-09 16:26:33 +0000

    重新整合服务端 添加HTTPSERVER 重置WEBSERVICE上下文 重置包名

---
+++ TankServer/src/lc/server/service/webservice/MsgEntry.java
@@ -0,0 +1,11 @@
+package lc.server.service.webservice;
+
+/**
+ * 消息实体
+ * @author LUCKY
+ *
+ */
+public class MsgEntry {
+	public boolean result;
+	public String resultMessage;
+}
---
+++ TankServer/src/lc/server/service/webservice/ServerWebServiceImpl.java
@@ -0,0 +1,29 @@
+package lc.server.service.webservice;
+
+import javax.jws.WebService;
+
+/**
+ * webservice实现类，依照接口的定义来发布公开的方法
+ * @author LUCKY
+ *
+ */
+//如果采用接口+实现类的方式，必须指定目标命名空间
+@WebService(targetNamespace="http://webservice.server.lc/",endpointInterface="lc.server.webservice.ServerWebService")
+public class ServerWebServiceImpl implements ServerWebService{
+
+	@Override
+	public MsgEntry login(String username, String password) {
+		// TODO Auto-generated method stub.
+		MsgEntry msg=new MsgEntry();
+		msg.result=false;
+		return msg;
+	}
+	@Override
+	public MsgEntry register(String username, String password) {
+		// TODO Auto-generated method stub
+		MsgEntry msg=new MsgEntry();
+		msg.result=true;
+		msg.resultMessage="成功";
+		return msg;
+	}	
+}
