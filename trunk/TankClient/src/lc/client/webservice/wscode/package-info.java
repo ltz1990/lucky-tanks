a551b7b3649e78d672859db545110c7e9eaed955
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-10 09:23:01 +0000

    webservice整理

--- trunk/TankClient/src/lc/client/webservice/wscode/package-info.java
+++ trunk/TankClient/src/lc/client/webservice/wscode/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.server.lc/")
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://webservice.service.server.lc/")
 package lc.client.webservice.wscode;
