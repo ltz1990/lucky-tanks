241b8e2d19b109aa79b7cd12d95c52d6b22b9151
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-15 04:52:34 +0000

    创建游戏client 1.0

--- TankClient/src/lc/client/webservice/RemoteServiceProxy.java
+++ TankClient/src/lc/client/webservice/RemoteServiceProxy.java
@@ -7,6 +7,7 @@
  */
 package lc.client.webservice;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.xml.namespace.QName;
@@ -25,25 +26,20 @@ import lc.client.webservice.wscode.ServerWebServiceImplService;
 public class RemoteServiceProxy{
 	private static RemoteServiceProxy remoteService;
 	private static ServerWebService proxy;
-	private RemoteServiceProxy(){
-		try {
+	private RemoteServiceProxy() throws Exception{
 			String url = "http://"+ClientConstant.SERVER_ADDRESS+":"+(ClientConstant.SERVER_PORT-1)+"/WebService?wsdl";
 			//ClientStart.class.getClassLoader().loadClass("lc.client.webservice.wscode.ServerWebServiceImplService");
 			Class.forName("lc.client.webservice.wscode.ServerWebServiceImplService", false, ClientStart.class.getClassLoader());
 			Class.forName("javax.xml.ws.spi.Provider", false, ClientStart.class.getClassLoader());
 			ServerWebServiceImplService service=new ServerWebServiceImplService(new URL(url), new QName("http://webservice.service.server.lc/", "ServerWebServiceImplService"));
 			proxy=service.getServerWebServiceImplPort();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 	/**
 	 * 获得远程代理
 	 * @return
 	 */
-	public static synchronized ServerWebService getInstance(){
+	public static synchronized ServerWebService getInstance() throws Exception{
 		if(remoteService==null||proxy==null){
 			remoteService=new RemoteServiceProxy();
 		}
