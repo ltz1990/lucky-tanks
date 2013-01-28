371d14c9403667bfe915c27167bf888935bf223d
ltz1990@gmail.com <ltz1990@gmail.com@e500c28c-97bc-d5ee-314d-d5f5f9d5f7d9>
2013-01-28 07:55:49 +0000

    备份

--- TankClient/src/lc/client/webservice/wscode/MsgEntry.java
+++ TankClient/src/lc/client/webservice/wscode/MsgEntry.java
@@ -1,8 +1,11 @@
 
 package lc.client.webservice.wscode;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -19,6 +22,7 @@ import javax.xml.bind.annotation.XmlType;
  *         &lt;element name="result" type="{http://www.w3.org/2001/XMLSchema}boolean"/>
  *         &lt;element name="resultMessage" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="object" type="{http://www.w3.org/2001/XMLSchema}anyType" minOccurs="0"/>
+ *         &lt;element name="objectList" type="{http://www.w3.org/2001/XMLSchema}anyType" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -31,22 +35,24 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "msgEntry", propOrder = {
     "result",
     "resultMessage",
-    "object"
+    "object",
+    "objectList"
 })
 public class MsgEntry {
 
     protected boolean result;
     protected String resultMessage;
     protected Object object;
-
-    public MsgEntry(){
-    	
-    }
+    @XmlElement(nillable = true)
+    protected List<Object> objectList;
+    
+    public MsgEntry(){};
     
     public MsgEntry(boolean result,String resultMessage){
     	this.result=result;
     	this.resultMessage=resultMessage;
     }
+
     /**
      * Gets the value of the result property.
      * 
@@ -109,6 +115,35 @@ public class MsgEntry {
      */
     public void setObject(Object value) {
         this.object = value;
+    }
+
+    /**
+     * Gets the value of the objectList property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the objectList property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getObjectList().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Object }
+     * 
+     * 
+     */
+    public List<Object> getObjectList() {
+        if (objectList == null) {
+            objectList = new ArrayList<Object>();
+        }
+        return this.objectList;
     }
 
 }
